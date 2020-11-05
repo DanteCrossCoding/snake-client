bb14bd17a4034464ff77f53576b90b9cb3c0696a
Dante Cross <dantecross@outlook.com>
2020-11-05 21:02:36 +0000

    adding movement to snek

--- input.js
+++ input.js
@@ -1,13 +1,33 @@
 const net = require('net');
+let connection;
 
-const setupInput = function() {
+const userInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }  
+  if ( key === w) {
+    conn.write('Move: up')
+  }
+  if ( key === s) {
+    conn.write('Move: down')
+  }
+  if ( key === a) {
+    conn.write('Move: left')
+  }
+  if ( key === d) {
+    conn.write('Move: right')
+  }
+};
+
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  if (key === '\u0003') {
-    process.exit();
-  }  
+  stdin.on('data', userInput)
+  
   return stdin;
 }
 
