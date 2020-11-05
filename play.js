2b795d56ac4578a963b90816e7f354da831af565
Dante Cross <dantecross@outlook.com>
2020-11-05 05:53:36 +0000

    not trapped snek

--- play.js
+++ play.js
@@ -3,3 +3,14 @@ const connect = require('./client')
 
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  if (key === '\u0003') {
+    process.exit();
+  }  
+  return stdin;
+}
